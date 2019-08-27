958b81a1fdaffc7eb7b971adbee99445f01c6be7
geonhwiii <g01063962671@gmail.com>
2019-08-27 20:25:35 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
+import UserList from './components/UserList';
+import CreateUser from './components/CreateUser';
+
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } = inputs;
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'sohee',
+      email: 'sohee@naver.com',
+      active: true,
+    },
+    {
+      id: 2,
+      username: 'dahyun',
+      email: 'dahyun@nate.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'nayun',
+      email: 'nayun@gmail.com',
+      active: false
+    }
+  ]);
+  const nextId = useRef(4);
+
+  const onChange = e => {
+    const { name, value } = e.target
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  }
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users,user]);
+    setInputs({
+      username: '',
+      email: ''
+    })
+    nextId.current += 1;
+  };
+
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id));
+  }
+
+  const onToggle = id => {
+    console.log('toggle')
+    setUsers(users.map(
+      user => user.id === id ? { ...user, active: !user.active } : user
+    ));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+    </>
   );
 }
 
---
+++ src/components/UserList.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function User({ user, onRemove, onToggle }) {
+  const { username, email, id, active } = user;
+  return (
+    <div>
+      <b onClick={() => onToggle(id)} style={{
+        color: active ? 'orange' : 'black',
+        cursor: 'pointer'
+      }}>
+        {username}
+      </b> 
+      &nbsp;
+      <span>({email})</span>
+      <button onClick={() => onRemove(id)}>삭제</button>
+    </div>
+  );
+}
+
+function UserList({ users, onRemove, onToggle }) {
+  return (
+    <div>
+      {
+        users.map(
+          user => (<User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>)
+        )
+      }
+    </div>
+  )
+}
+export default UserList;
